33316f644e38fb40c83c940509d3e8a7984cdb33
MertBilecen <mertcenk71@hotmail.com>
2024-08-21 20:20:28 +0300

    Login Testi eklendi (Sana gelen işin adı, Jiradan gelen)

---
+++ src/Gün01/Merhaba.java
@@ -0,0 +1,12 @@
+package Gün01;
+
+public class Merhaba {
+    public static void main(String[] args) {
+
+        System.out.println("Merhaba 1");
+
+        System.out.println("Merhaba 2");
+
+        System.out.println("Task 1 kodları");
+    }
+}
